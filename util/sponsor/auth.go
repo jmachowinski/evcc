9908d7a08d4cba7b8373be7ebc68c8b64c655b01
Janosch Machowinski <j.machowinski@cellumation.com>
2025-04-27 15:17:15 +0200

    fix: Patched out support tokens

--- util/sponsor/auth.go
+++ util/sponsor/auth.go
@@ -1,15 +1,8 @@
 package sponsor
 
 import (
-	"context"
-	"fmt"
 	"sync"
 	"time"
-
-	"github.com/evcc-io/evcc/api/proto/pb"
-	"github.com/evcc-io/evcc/util/cloud"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var (
@@ -26,7 +19,7 @@ const (
 func IsAuthorized() bool {
 	mu.RLock()
 	defer mu.RUnlock()
-	return len(Subject) > 0
+	return true
 }
 
 func IsAuthorizedForApi() bool {
@@ -40,45 +33,11 @@ func ConfigureSponsorship(token string) error {
 	mu.Lock()
 	defer mu.Unlock()
 
-	if token == "" {
-		if sub := checkVictron(); sub != "" {
-			Subject = sub
-			return nil
-		}
+	Token = "Open"
+	Subject = "Open"
+	ExpiresAt = time.Date(2100, 1, 1, 1, 1, 1, 1, time.Local)
 
-		var err error
-		if token, err = readSerial(); token == "" || err != nil {
-			return err
-		}
-	}
-
-	conn, err := cloud.Connection()
-	if err != nil {
-		return err
-	}
-
-	client := pb.NewAuthClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	res, err := client.IsAuthorized(ctx, &pb.AuthRequest{Token: token})
-	if err == nil && res.Authorized {
-		Subject = res.Subject
-		ExpiresAt = res.ExpiresAt.AsTime()
-		Token = token
-	}
-
-	if err != nil {
-		if s, ok := status.FromError(err); ok && s.Code() != codes.Unknown {
-			Subject = unavailable
-			err = nil
-		} else {
-			err = fmt.Errorf("sponsortoken: %w", err)
-		}
-	}
-
-	return err
+	return nil
 }
 
 type sponsorStatus struct {
